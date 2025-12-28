9b7eb3b0f7da33e0484738c5542ef6aec72d5677
P1YU5H-50N1 <me.piyush.360@gmail.com>
2025-12-28 22:46:46 +0530

    updated constants.py

---
+++ CONSTANTS.py
@@ -0,0 +1,6 @@
+import os
+
+SMTP_PORT = os.environ.get("SMTP_PORT") 
+SMTP_SERVER = os.environ.get("SMTP_SERVER") 
+CONVEYOR_EMAIL = os.environ.get("CONVEYOR_EMAIL")
+PASSWORD = os.environ.get("PASSWORD")
